a9f6b3a51de5ec21db509eb393e328e8ac953584
noaboa07 <143129234+noaboa07@users.noreply.github.com>
2024-10-05 16:50:34 -0600

    Step 4 Complete
    Cards are all shown, rotated for each card, structs made, etc.

--- Flashcard/Flashcard/ContentView.swift
+++ Flashcard/Flashcard/ContentView.swift
@@ -8,37 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var cards: [Card] = Card.mockedCards
     var body: some View {
-        
-        ZStack {
+    
 
-            // Card background
-            RoundedRectangle(cornerRadius: 25.0)
-                .fill(Color.blue.gradient)
-                .shadow(color: .black, radius: 4, x: -2, y: 2)
-            
-            VStack(spacing: 20) {
-                
-                // Card type (question vs answer)
-                Text("Question")
-                    .bold()
-                
-                // Separator
-                   Rectangle()
-                    .frame(height: 1)
-                
-                // Card text
-                Text("Located at the southern end of Puget Sound, what is the capitol of Washington?")
+            // Card deck
+            ZStack {
+                ForEach(0..<cards.count, id: \.self) { index in
+                    CardView(card: cards[index])
+                        .rotationEffect(.degrees(Double(cards.count - 1 - index) * -5))
+                }
             }
-            .font(.title)
-            .foregroundStyle(.white)
-            .padding()
         }
-        .frame(width: 300, height: 500)
-
-    
-    
-    }
 }
 
 #Preview {
