677b340079ae200b050a3caf1b579233c2da3c47
noaboa07 <143129234+noaboa07@users.noreply.github.com>
2024-10-05 16:40:46 -0600

    Steps 1-3 Complete

---
+++ Flashcard/Flashcard/CardView.swift
@@ -0,0 +1,50 @@
+import SwiftUI
+
+struct CardView: View {
+        let card: Card
+    @State private var isShowingQuestion = true
+    
+    var body: some View {
+        ZStack {
+
+            // Card background
+            RoundedRectangle(cornerRadius: 25.0)
+                .fill(isShowingQuestion ? .blue : .indigo)
+                .shadow(color: .black, radius: 4, x: -2, y: 2)
+
+            VStack(spacing: 20) {
+
+                // Card type (question vs answer)
+                Text(isShowingQuestion ? "Question" : "Answer")
+                    .bold()
+
+                // Separator
+                Rectangle()
+                    .frame(height: 1)
+
+                // Card text
+                Text(isShowingQuestion ? card.question : card.answer)
+               
+            }
+            .font(.title)
+            .foregroundStyle(.white)
+            .padding()
+        }
+        .frame(width: 300, height: 500)
+        .onTapGesture {
+            isShowingQuestion.toggle()
+        }
+    }
+}
+
+#Preview {
+    CardView(card: Card(
+        question: "Located at the southern end of Puget Sound, what is the capitol of Washington?",
+        answer: "Olympia"))
+}
+
+// Card data model
+struct Card {
+    let question: String
+    let answer: String
+}
--- Flashcard/Flashcard/ContentView.swift
+++ Flashcard/Flashcard/ContentView.swift
@@ -9,13 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+
+            // Card background
+            RoundedRectangle(cornerRadius: 25.0)
+                .fill(Color.blue.gradient)
+                .shadow(color: .black, radius: 4, x: -2, y: 2)
+            
+            VStack(spacing: 20) {
+                
+                // Card type (question vs answer)
+                Text("Question")
+                    .bold()
+                
+                // Separator
+                   Rectangle()
+                    .frame(height: 1)
+                
+                // Card text
+                Text("Located at the southern end of Puget Sound, what is the capitol of Washington?")
+            }
+            .font(.title)
+            .foregroundStyle(.white)
+            .padding()
         }
-        .padding()
+        .frame(width: 300, height: 500)
+
+    
+    
     }
 }
 
