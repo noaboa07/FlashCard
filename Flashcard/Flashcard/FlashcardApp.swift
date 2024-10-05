62d42156a67f5a2776cce6c25c8a0da92ee520a4
noaboa07 <143129234+noaboa07@users.noreply.github.com>
2024-10-05 16:09:14 -0600

    Initial commit

---
+++ Flashcard/Flashcard/FlashcardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardApp.swift
+//  Flashcard
+//
+//  Created by Tamara Russell on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
